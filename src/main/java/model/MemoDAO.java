1ad681cd507fa71dcb1f2d07cfad078cfc13c7f4
iida <iidahirokazu2@gmail.com>
2024-08-29 18:45:41 +0900

    insert-memo機能追加、セッションに渡す引数をLoginIdに変更

--- src/main/java/model/MemoDAO.java
+++ src/main/java/model/MemoDAO.java
@@ -3,7 +3,6 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import bean.MemoBean;
 
@@ -13,38 +12,36 @@ public class MemoDAO {
 	final String USER="root";
 	final String PASS="pass";
 	
-//	Connection con = null;
-//	PreparedStatement ps = null;
-//	ResultSet rs = null;
-//	String sql = null;
-	
 	public void insertMemo(MemoBean mb) {
 		
 		Connection con = null;
 		PreparedStatement ps = null;
-		ResultSet rs = null;
 		String sql = null;
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection(URL, USER, PASS);
-			System.out.println("接続OK");
+			System.out.println("memo 接続OK");
 			sql="insert into memos(user_id,title,content) values(?,?,?)";
 			
 			ps = con.prepareStatement(sql);
 			ps.setString(1, mb.getMemoUserId());
 			ps.setString(2, mb.getMemoTitle());
 			ps.setString(3, mb.getMemoContent());
-			rs = ps.executeQuery();
-			System.out.print(rs.getString(1 + ":"));
-			System.out.print(rs.getString(2 + ":"));
-			System.out.print(rs.getString(3));
+			
+			System.out.print("ps:" + mb.getMemoUserId() + ":");
+			System.out.print(mb.getMemoTitle() + ":");
+			System.out.println(mb.getMemoContent());
+			
+			ps.executeUpdate();
+			
+			System.out.println("insertMemoメソッド ps update OK");
+
 			
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally {
 	    	try{
-    		if(rs != null) rs.close();
     		if(ps != null) ps.close();
     		if(con != null) con.close();
     		} catch(Exception e){
