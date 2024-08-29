4088b82b4deb7d576b6edc3a18c0ec1c2e275c49
iida <iidahirokazu2@gmail.com>
2024-08-29 14:41:57 +0900

    MemoBean追加

---
+++ src/main/java/bean/MemoBean.java
@@ -0,0 +1,29 @@
+package bean;
+
+public class MemoBean {
+	private String memoTitle;
+	private String memoContent;
+	private String memoUserId;
+	
+	public void setMemoTitle(String memoTitle) {
+		this.memoTitle = memoTitle;
+	}
+	public String getMemoTitle() {
+		return memoTitle;
+	}
+	
+	public void setMemoContent(String memoContent) {
+		this.memoContent = memoContent;
+	}
+	public String getMemoContent() {
+		return memoContent;
+	}
+	
+	public void setMemoUserId(String memoUserId) {
+		this.memoUserId = memoUserId;
+	}
+	public String getMemoUserId() {
+		return memoUserId;
+	}
+
+}
